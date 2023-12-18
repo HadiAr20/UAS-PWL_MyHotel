d08fda5b59f3b42b5905acac3c1e16bb029781f8
HadiAr20 <hadimuhamad426@gmail.com>
2023-12-18 22:01:27 +0700

    update

--- Frontend/src/pages/Admin/Kamar/List_Kamar.jsx
+++
@@ -1,9 +0,0 @@
-function ListKamar() {
-    return (
-      <div className="App">
-        <h1>Simple Todo App</h1>
-      </div>
-    );
-  }
-  
-  export default ListKamar;
--- Frontend/src/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-import ListKamar from "./Admin/Kamar/List_Kamar";
-import EditKamar from "./Admin/Kamar/Edit_Kamar";
-import 'bootstrap/dist/css/bootstrap.min.css'
-export {ListKamar,EditKamar};
